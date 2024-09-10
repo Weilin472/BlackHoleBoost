c5c2ebc6999823e1c629c46339193d1fa4943575
Justin Lam <60529932+Excalabreak@users.noreply.github.com>
2024-09-10 09:51:17 -0700

    start on pick up system

---
+++ BlackHoleBoost/Assets/Scripts/Asteroid/SmallAsteroid/Pickup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [09/10/2024]
+ * [pick up for when player flies over item]
+ */
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] private SmallAsteroidType _type = SmallAsteroidType.NORMAL;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.gameObject.GetComponent<PlayerAsteroidInventory>())
+        {
+            
+        }
+    }
+}
---
+++ BlackHoleBoost/Assets/Scripts/Asteroid/SmallAsteroid/SmallAsteroidType.cs
@@ -0,0 +1,12 @@
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [09/10/2024]
+ * [enum for small asteroid]
+ */
+
+public enum SmallAsteroidType
+{
+    NORMAL,
+    BOUNCE,
+    STICKY
+}
