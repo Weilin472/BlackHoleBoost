06ef072ab173b9890e4037f6dac8277fb488ecfc
Justin Lam <60529932+Excalabreak@users.noreply.github.com>
2024-10-22 09:45:46 -0700

    start move

---
+++ BlackHoleBoost/Assets/Scripts/Asteroid/PickupSmallAsteroid/PickupSmallAsteroidMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [10/22/2024]
+ * [movement script for pickup asteroids]
+ */
+
+public class PickupSmallAsteroidMove : MonoBehaviour
+{
+    private bool magnet = true;
+}
