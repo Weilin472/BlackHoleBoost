28cd29c061a85717208a4de147a451bb1c637389
Weilin471 <wtang1@mail.csuchico.edu>
2024-10-29 08:47:58 -0700

    enemyindicator

--- BlackHoleBoost/Assets/Scripts/Health/EnemyHealthScript.cs
+++ BlackHoleBoost/Assets/Scripts/Health/EnemyHealthScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyHealthScript : BaseHealthScript
 {
+    private bool isBeingDamaged;
     private void Awake()
     {
         ResetHealth();
@@ -12,5 +13,42 @@ public class EnemyHealthScript : BaseHealthScript
     protected override void OnDeath()
     {
         Destroy(gameObject);
+    }
+
+    public override void Damage(int damage)
+    {
+        if (!_invincible)
+        {
+            _currentHealth -= damage;
+            
+            if (_currentHealth <= 0)
+            {
+                _currentHealth = 0;
+                OnDeath();
+                return;
+            }
+            if (!isBeingDamaged)
+            {
+                StartCoroutine(DamageIndicator());
+            }
+        }
+    }
+
+    private IEnumerator DamageIndicator()
+    {
+        isBeingDamaged = true;
+        Dictionary<MeshRenderer, Color> originalColor = new Dictionary<MeshRenderer, Color>();
+        MeshRenderer[] renderer = transform.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < renderer.Length; i++)
+        {
+            originalColor.Add(renderer[i], renderer[i].material.color);
+            renderer[i].material.color = Color.red;
+        }
+        yield return new WaitForSeconds(0.5f);
+        foreach (var item in originalColor)
+        {
+            item.Key.material.color = item.Value;
+        }
+        isBeingDamaged = false;
     }
 }
