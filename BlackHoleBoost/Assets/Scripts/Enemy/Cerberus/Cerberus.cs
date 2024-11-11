6bc86f63c4bf649196bd9c41c3f72b9b84bf2e60
Justin Lam <jlam123321@gmail.com>
2024-11-11 00:49:10 -0800

    cerberus done

--- BlackHoleBoost/Assets/Scripts/Enemy/Cerberus/Cerberus.cs
+++ BlackHoleBoost/Assets/Scripts/Enemy/Cerberus/Cerberus.cs
@@ -2,17 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cerberus : MonoBehaviour
+public class Cerberus : EnemyBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _guardSpeed;
+    [SerializeField] private float _pursuitSpeed;
+    [SerializeField] private float _distanceToPursuit;
+
+    private bool _pursuit = false;
+
+    private Vector3 _guardPos;
+    private Vector3 _nextPos;
+
+    /// <summary>
+    /// sets the speed
+    /// sets a random guard position
+    /// </summary>
+    private void OnEnable()
     {
-        
+        _speed = _guardSpeed;
+        float[] xPos = { -4.25f, 0f, 4.25f };
+
+        _guardPos = new Vector3(xPos[Random.Range(0, 3)], 0, 0);
+        _nextPos = _guardPos;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// only moves to player when in persuit mode
+    /// </summary>
+    protected override void Movement()
     {
-        
+        if (_pursuit)
+        {
+            base.Movement();
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        CheckForPersuit();
+        if (!_pursuit)
+        {
+            MoveToNextPos();
+        }
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, _rigid.velocity);
+    }
+
+    private void CheckForPersuit()
+    {
+        Vector3 playerPos = Vector3.zero;
+        if (GameManager.Instance._inPrototype)
+        {
+            if (GameManager.Instance.players.Count > 0)
+            {
+                playerPos = GameManager.Instance.GetPlayerWithMoreHealth().transform.position;
+            }
+        }
+        else
+        {
+            if (GameObject.FindGameObjectWithTag("Player"))
+            {
+                playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+            }
+
+
+        }
+
+        float dist = Vector3.Distance(transform.position, playerPos);
+        if (dist <= _distanceToPursuit)
+        {
+            _pursuit = true;
+            _speed = _pursuitSpeed;
+        }
+    }
+
+    private void MoveToNextPos()
+    {
+        if ((_nextPos - transform.position).magnitude < .1f)
+        {
+            Vector3 nextLocFromGuardPos = Random.insideUnitSphere * 5;
+            nextLocFromGuardPos.z = 0;
+            _nextPos = _guardPos + nextLocFromGuardPos;
+        }
+
+        Vector3 dir = (_nextPos - transform.position).normalized;
+        _rigid.velocity = dir * _currentSpeed;
     }
 }
